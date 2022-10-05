faf63c94db1a406a33ec79e2fdc09fee63777971
renydiaz02 <89815150+renydiaz02@users.noreply.github.com>
2022-10-05 01:00:01 -0400

    Add project files.

---
+++ ArticuloApi/data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using ArticuloApi.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArticuloApi.data
+{
+    public class Contexto:DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+        }
+
+        public DbSet<Articulos> Articulos { get; set; }
+    }
+}
